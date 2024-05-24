1a1b846dc4661f49f3a69a99298f64cef8fda855
Vikas Magar <vikas121magar@gmail.com>
2024-05-24 19:53:07 +0530

    initial commit with project completed

---
+++ ThapaEcom/getCartProducts.js
@@ -0,0 +1,15 @@
+import {updateCartValue} from "./updateCartValue";
+
+export const getCartProductFromLS = () => {
+  let cartProducts = localStorage.getItem('cartProductLS')
+  if (!cartProducts) {
+    return []
+  }
+  cartProducts = JSON.parse(cartProducts)
+
+//   update cart button value
+
+updateCartValue(cartProducts);
+
+  return cartProducts
+};
